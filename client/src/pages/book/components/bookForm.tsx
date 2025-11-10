42600a4467bd119e031f7aef35e2736644a3b3c6
HezerSantos <HezerSantos24@gmail.com>
2025-11-09 21:46:02 -0600

    Added book data

--- client/src/pages/book/components/bookForm.tsx
+++ client/src/pages/book/components/bookForm.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import BookInput from "./bookInput"
 
 interface BookFormProps {
@@ -5,15 +6,19 @@ interface BookFormProps {
     times: string[]
 }
 const BookForm: React.FC<BookFormProps> = ({selectedDate, times}) => {
+    const [ bookData, setBookData ] = useState<Record<string, any> | null>(null)
+    useEffect(() => {
+        console.log(bookData)
+    }, [bookData])
     return(
         <>
             <div className="book-form">
                 <h1>Book Your Session</h1>
                 <div className="book-form-inputs">
-                    <BookInput label="Date" name="date" type="date" readonly={true} selectedDate={selectedDate}/>
-                    <BookInput label="Time" name="time" type="" select={true} times={times}/>
-                    <BookInput label="Name" name="name" type="text"/>
-                    <BookInput label="Email" name="email" type="text"/>
+                    <BookInput label="Date" name="date" type="date" setBookData={setBookData} readonly={true} selectedDate={selectedDate}/>
+                    <BookInput label="Time" name="time" type="" setBookData={setBookData} select={true} times={times}/>
+                    <BookInput label="Name" name="name" setBookData={setBookData} type="text"/>
+                    <BookInput label="Email" name="email" setBookData={setBookData} type="text"/>
                 </div>
                 <p>*A $25 deposit is required to book an appointment</p>
                 <div className="book-form-footer">
--- client/src/pages/book/components/bookInput.tsx
+++ client/src/pages/book/components/bookInput.tsx
@@ -1,24 +1,47 @@
-import { useState } from "react"
+import React, { SetStateAction, useEffect, useState } from "react"
 
 interface BookInputProps {
     label: string
     name: string
     type: string
+    setBookData: React.Dispatch<SetStateAction<Record<string, any> | null>>
     readonly?: boolean
     selectedDate?: Date | null
     select?: boolean
     times?: string[]
 }
 
-const BookInput: React.FC<BookInputProps> = ({label, name, type, readonly, selectedDate, select, times}) => {
+const BookInput: React.FC<BookInputProps> = ({label, name, type, setBookData, readonly, selectedDate, select, times}) => {
     const date = selectedDate? selectedDate.toISOString().split('T')[0] : null
     const [value, setValue] = useState("")
+    const [ selectValue, setSelectValue ] = useState("")
+    useEffect(() => {
+        setBookData(prev => {
+            const newBookData = {...prev}
+            newBookData[name] = value
+            return newBookData
+        })
+    }, [value])
+    useEffect(() => {
+        setBookData(prev => {
+            const newBookData = {...prev}
+            newBookData[name] = selectValue
+            return newBookData
+        })
+    }, [selectValue])
+    useEffect(() => {
+        setBookData(prev => {
+            const newBookData = {...prev}
+            newBookData[name] = date
+            return newBookData
+        })
+    }, [date])
     if (select){
         return (
             <>
                 <div className="book-input">
                     <label htmlFor={label}>{label}</label>
-                    <select name={name} id={label}>
+                    <select name={name} id={label} value={selectValue} onChange={(e) => setSelectValue(e.target.value)}>
                         {times?.map((time, i) => {
                             return(
                                 <option value={time} key={i}>{time}</option>
